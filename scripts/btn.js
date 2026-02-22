552447c6873fc752a5b5c6c7b0133fe9c2f329ce
ihriyad <ihriyad.dev@gmail.com>
2026-02-22 23:46:07 +0600

    added basic function

---
+++ scripts/btn.js
@@ -0,0 +1,13 @@
+
+const buttons = document.querySelectorAll(".btn")
+// console.log(buttons)
+buttons.forEach(function(button){
+    // console.log(button)
+    button.addEventListener('click',function(){
+        buttons.forEach(function(btn){
+            // console.log(btn)
+            btn.classList.remove("active")
+        })
+        button.classList.add("active")
+    })
+})
---
+++ scripts/count.js
@@ -0,0 +1,29 @@
+const dashBoardTotal = document.getElementById("dashboard-total");
+const availableJobTotal = document.getElementById("available-job-total");
+// console.log(availableJobTotal)
+// console.log(dashBoardTotal)
+function updateCount() {
+  const allCards = document.querySelectorAll(".job-card");
+  const noJob = document.getElementById("no-job");
+  // console.log(allCards);
+  dashBoardTotal.innerText = allCards.length;
+  availableJobTotal.innerText = allCards.length;
+
+  if (allCards.length === 0) {
+    noJob.classList.remove("hidden");
+  } else {
+    noJob.classList.add("hidden");
+  }
+}
+updateCount();
+const deleteButtons = document.querySelectorAll(".delete-btn");
+deleteButtons.forEach((btn) => {
+  //   console.log(btn);
+  btn.addEventListener("click", function () {
+    const card = btn.closest(".job-card");
+    // console.log(card)
+    card.remove();
+
+    updateCount();
+  });
+});
